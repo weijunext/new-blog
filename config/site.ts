26aec0b49e9c7ceb9aa32fb3c052021f13aeeace
WEI JUN <weijunext@gmail.com>
2024-05-23 16:27:03 +0800

    feat: rss.xml

--- config/site.ts
+++ config/site.ts
@@ -3,21 +3,27 @@ import { BsGithub, BsTwitterX, BsWechat } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { SiBuymeacoffee, SiJuejin } from "react-icons/si";
 
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME;
+const SITE_DESCRIPTION = process.env.NEXT_PUBLIC_SITE_DESCRIPTION;
+const AUTHOR_NAME = process.env.NEXT_PUBLIC_AUTHOR_NAME;
+const TWITTER_USERNAME = process.env.NEXT_PUBLIC_TWITTER_USERNAME;
+
 const baseSiteConfig = {
-  name: "J实验室",
+  name: SITE_NAME || "J实验室",
   description:
-    "全栈工程师，Next.js开源手艺人，掘金签约作者，折腾才有未来",
-  url: "https://weijunext.com",
+    SITE_DESCRIPTION || "全栈工程师，Next.js开源手艺人，掘金签约作者，折腾才有未来",
+  url: SITE_URL || "https://weijunext.com",
   metadataBase: '/',
   keywords: ["前端工程师", "前端", "全栈工程师", "全栈", "独立开发者", "indie hacker", "react", "Next.js"],
   authors: [
     {
-      name: "weijunext",
+      name: AUTHOR_NAME || 'weijunext',
       url: "https://weijunext.com",
-      twitter: 'https://twitter.com/weijunext',
+      twitter: `https://x.com/${TWITTER_USERNAME || 'weijunext'}`,
     }
   ],
-  creator: '@weijunext',
+  creator: `@${AUTHOR_NAME}`,
   defaultNextTheme: 'dark', // next-theme option: system | dark | light
   verification: {
     google: process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '',

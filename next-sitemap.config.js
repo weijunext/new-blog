c43b04a46e95a098ca2af49524fc8c05992a28f0
WEI JUN <weijunext@gmail.com>
2024-07-02 09:24:08 +0800

    feat: 更新sitemap和robots规则

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,14 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://new.weijunext.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: ["**/*.webp"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/assets/"],
+      },
+    ],
+  },
 };

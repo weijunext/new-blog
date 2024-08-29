5a98ab8541c7d2e2fcc86c3d13f9b22614b51d3f
WEI JUN <weijunext@gmail.com>
2024-08-29 10:48:32 +0800

    feat: 修改网址

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://new.weijunext.com",
+  siteUrl: process.env.SITE_URL || "https://weijunext.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ["**/*.webp"],
